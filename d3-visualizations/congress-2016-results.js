49d3f0153a50c41078b4f48ec3e28de365a65b56
Mason Chinkin <mason.chinkin@gmail.com>
2018-05-20 20:25:48 -0700

    continuing work on congress map
    initial fill is finished

--- d3-visualizations/congress-2016-results.js
+++ d3-visualizations/congress-2016-results.js
@@ -10,7 +10,10 @@ var projection = d3.geoAlbers()
 //define drag behavior
 var zoom = d3.zoom()
     .scaleExtent([0.5, 10])
-    .on('zoom', zooming);
+    .on('zoom', function(d) {
+        map.style("stroke-width", 1 / d3.event.transform.k + "px");
+        map.attr("transform", d3.event.transform);
+    });
 
 // define path
 var path = d3.geoPath()
@@ -21,7 +24,7 @@ var svg = d3.select('#container')
     .append('svg')
     .attr('width', w)
     .attr('height', h)
-    .style('background', '#a6d0ef')
+    .style('background', '#e8e8e8')
     .style('border-style', 'solid')
     .style('border-color', 'grey');
 
@@ -38,121 +41,128 @@ map.append('rect')
     .attr('height', h)
     .attr('opacity', 0);
 
-// Define the div for the tooltip
-var tooltipDiv = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-d3.json('viz-data/us_congress_2016_lower_48.json', function(error, json) {
+d3.csv('viz-data/congress_results_2016.csv', function(error, data) {
     if (error) throw error;
-    /*
-            //merge the data
-            for (var i = 0; i < data.length; i++) {
-                var dataStateDist = data[i].STATEFP + '-Congressional District ' + data[i].DISTRICT;
+    //console.log(data)
 
-                var dataName = data[i].STATE;
+    d3.json('viz-data/us_congress_2016_lower_48.json', function(error, json) {
+        if (error) throw error;
+        //console.log(json);
 
-                for (var j = 0; j < json.features.length; j++) {
-                    var jsonStateDist = json.features[j].properties.STATEFP + '-' + json.features[j].properties.NAMELSAD;
+        //loop through, merging ag data with map
+        for (var i = 0; i < data.length; i++) {
 
-                    //console.log(jsonState);
-                    if (dataStateDist == jsonStateDist) {
-                        //copy data from csv to json
-                        json.features[j].properties.state = data[i].STATE;
-                        json.features[j].properties.district = data[i].DISTRICT;
-                        //stop looking through json
-                        break;
+            var dataDistrict = data[i].state_fips + '-' + data[i].district;
+            var dataDistrictWinner = data[i].winner
+            var dataDistrictWinningParty = data[i].party
+            var dataDistrictWinningMargin = parseFloat(data[i].general_perc)
+
+            var dataParty = data[i].party;
+
+            for (var j = 0; j < json.features.length; j++) {
+
+                var jsonDistrict = json.features[j].properties.STATEFP + '-' + json.features[j].properties.CD115FP;
+
+                if (dataDistrict == jsonDistrict && dataDistrictWinner == 'W') {
+                    //copy the data from csv to json
+                    json.features[j].properties.district = jsonDistrict;
+                    json.features[j].properties.winningParty = dataDistrictWinningParty;
+                    json.features[j].properties.winningMargin = dataDistrictWinningMargin;
+
+                    //stop looking through json
+                    break;
+                }
+            }
+        }
+
+        console.log(json);
+
+        //bind data and create one path per json feature (state)
+        map.selectAll('path')
+            .data(json.features)
+            .enter()
+            .append('path')
+            .attr("d", path)
+            .style('fill', function(d) {
+                if (d.properties.winningParty == 'R') { return 'red' } else { return 'blue' }
+            })
+            .style('opacity', function(d) {
+                return d.properties.winningMargin;
+            })
+            .style('stroke', 'white')
+            .attr('class', function(d) { return d.properties.district })
+            .on('mouseover', function(d) {
+
+                d3.select(this)
+                    .transition()
+                    .duration(50)
+                    .style('fill', 'orange');
+
+                // Define the div for the tooltip
+                tooltipDiv = d3.select("body").append("div")
+                    .attr("class", "tooltip")
+                    .style("opacity", 0);
+
+                tooltipDiv.transition()
+                    //.duration(200)
+                    .style("opacity", .9)
+                    .style("left", (d3.event.pageX + 5) + "px")
+                    .style("top", (d3.event.pageY - 38) + "px");
+
+                thisJsonDistrict = d3.select(this).attr('class');
+                //console.log(thisJsonDistrict);
+
+                for (var i = 0; i < data.length; i++) {
+                    thisDataDistrict = data[i].state_fips + '-' + data[i].district;
+                    //console.log(thisDataDistrict);
+
+                    if (thisJsonDistrict == thisDataDistrict) {
+                        console.log(data[i].candidate + " " + i);
+
+                        //var iReset = i
+
+                        //tooltipDiv.data(data)
+                        //.enter()
+                        //.append('g')
+                        //.attr("class", "tooltip")
+                        //.attr("transform", function(d, i) { { return "translate(0," + i * 20 + ")"; }
+                        //})
+                        ;
+
+                        /*tooltipDiv.append('text')
+                            //.attr("dy", ".35em")
+                            .text(function(d, i) { data[i].candidate; })
+                            .attr("class", "tooltip text");
+                        
+                                                tooltip.append('rect')
+                                                    .attr("x", wLegend)
+                                                    .attr("y", hLegend + 30)
+                                                    .attr("width", 12)
+                                                    .attr("height", 12)
+                                                    .style("fill", function(d, i) {
+                                                        if (d.party == 'R' && d.winner == 'W') { return 'red' } else { return 'blue' }
+                                                    });*/
+
                     }
                 }
-            }*/
-
-    //console.log(json);
-
-    //bind data and create one path per json feature (state)
-    map.selectAll('path')
-        .data(json.features)
-        .enter()
-        .append('path')
-        .attr("d", path)
-        .style('fill', 'beige')
-        .style('stroke', 'grey')
-        .attr('class', function(d) { return d.properties.STATEFP + '-' + d.properties.CD115FP; })
-        .on('mousemove', mouseMove)
-        .on('mouseout', mouseOut);
-});
-
-function zooming() {
-    map.style("stroke-width", 1 / d3.event.transform.k + "px");
-
-    map.attr("transform", d3.event.transform);
-}
-
-var mouseMove = function(data) {
-    d3.select(this)
-        .transition()
-        .duration(50)
-        .style('fill', 'orange');
-
-    thisDistrict = d3.select(this).attr('class');
-    console.log(thisDistrict);
-
-    //trip data
-    d3.csv('viz-data/congress_results_2016.csv', function(error, data) {
-        if (error) throw error;
-
-        for (var i = 0; i < data.length; i++) {
-            if (thisDistrict == data[i].STATEFP + '-' + data[i].DISTRICT) {
-
-                console.log(data[i].CANDIDATE_NAME);
-
-                /*wLegend = (d3.event.pageX + 5) + "px";
-                hLegend = (d3.event.pageY - 38) + "px";
-
-                var legend = svg.selectAll('.legend')
-                    .data(data)
-                    .enter()
-                    .append('g')
-                    .attr("class", "legend")
-                    .attr("transform", function(d, i) { { return "translate(0," + i * 20 + ")"; }
+            })
+            .on('mouseout', function(d) {
+                d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .style('fill', function(d) {
+                        if (d.properties.winningParty == 'R') { return 'red' } else { return 'blue' }
                     });
 
-                legend.append('rect')
-                    .attr("x", wLegend)
-                    .attr("y", hLegend + 30)
-                    .attr("width", 12)
-                    .attr("height", 12)
-                    .style("fill", function(d, i) {
-                        if (d.PARTY == 'R' && d.WINNER == 'W') { return 'red' } else { return 'blue' }
-                    });
+                d3.selectAll('.tooltip')
+                    .remove();
 
-                legend.append('text')
-                    .attr("x", wLegend + 20)
-                    .attr("y", hLegend + 42)
-                    //.attr("dy", ".35em")
-                    .text(function(d, i) { d.CANDIDATE_NAME; })
-                    .attr("class", "textselected");*/
-            };
-        };
+                tooltipDiv.transition()
+                    //.duration(200)
+                    .style("opacity", .0);
+            });
     });
-
-    tooltipDiv.transition()
-        //.duration(200)
-        .style("opacity", .9)
-        .style("left", (d3.event.pageX + 5) + "px")
-        .style("top", (d3.event.pageY - 38) + "px");
-};
-
-//properties of mouseout
-var mouseOut = function(d) {
-    d3.select(this)
-        .transition()
-        .duration(100)
-        .style('fill', 'beige');
-
-    tooltipDiv.transition()
-        //.duration(200)
-        .style("opacity", .0);
-};
+});
 
 function getWidth() {
     return Math.max(
@@ -173,6 +183,3 @@ function getHeight() {
         document.documentElement.clientHeight
     );
 }
-
-//console.log('Width:  ' + getWidth());
-//console.log('Height: ' + getHeight());
